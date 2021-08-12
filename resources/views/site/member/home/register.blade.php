41d6dc34c61bf5b9e8d45451afebb85c8976c14f
Samia <samiamhassouna@gmail.com>
2021-08-13 02:20:52 +0300

    Update register.blade.php

--- resources/views/site/member/home/register.blade.php
+++ resources/views/site/member/home/register.blade.php
@@ -1,28 +1,28 @@
 <?php 
-$countries = [
-    "SA" =>'المملكة العربية السعودية',
-    "JO" =>'الأردن',
-    "AE" =>'الإمارات العربية المتحدة',
-    "DZ" =>'الجزائر',
-    "SD" =>'السودان',
-    "SO" =>'الصومال',
-    "IQ" =>'العراق',
-    "KW" =>'الكويت',
-    "MA" =>'المغرب',
-    "YE" =>'اليمن',
-    "TN" =>'تونس',
-    "GI" =>'جبل طارق',
-    "KM" =>'جزر القمر',
-    "SY" =>'سوريا',
-    "OM" =>'عمان',
-	"PL" =>'فلسطين',
-    "QA" =>'قطر',
-    "LB" =>'لبنان',
-    "LY" =>'ليبيا',
-    "EG" =>'مصر',
-    "BH" =>'مملكة البحرين',
-];
-?>
+  $countries = [
+      "SA" =>'المملكة العربية السعودية',
+      "JO" =>'الأردن',
+      "AE" =>'الإمارات العربية المتحدة',
+      "DZ" =>'الجزائر',
+      "SD" =>'السودان',
+      "SO" =>'الصومال',
+      "IQ" =>'العراق',
+      "KW" =>'الكويت',
+      "MA" =>'المغرب',
+      "YE" =>'اليمن',
+      "TN" =>'تونس',
+      "GI" =>'جبل طارق',
+      "KM" =>'جزر القمر',
+      "SY" =>'سوريا',
+      "OM" =>'عمان',
+  	"PL" =>'فلسطين',
+      "QA" =>'قطر',
+      "LB" =>'لبنان',
+      "LY" =>'ليبيا',
+      "EG" =>'مصر',
+      "BH" =>'مملكة البحرين',
+  ];
+  ?>
 @extends('../layouts.include-site')
 @section('title', 'تسجيل مشترك') 
 @section('content')
@@ -62,33 +62,32 @@ $countries = [
                     <input class="form-control nice-select @error('birthday') is-invalid @enderror" required maxlength="50" style="width:45%" class=" arabic form-control " placeholder="تاريخ الميلاد" name="birthday" value="{{old('birthday')}}" type="date" req>
                   </div>
                   <br>
-                      <div class="form-group mt-4 mb-4 @error('country') is-invalid @enderror">
-                  <select name="country" required class="form-control">
+                  <div class="form-group mt-4 mb-4 @error('country') is-invalid @enderror">
+                    <select name="country" required class="form-control">
                       <option>اختر الدولة</option>
-@foreach($countries as $country)                      
-<option {{old('country') == $country ? 'selected':''}} value="{{$country}}">{{$country}}</option>
-@endforeach        
-</select>
-               </div>
-                    <br><br><br><br>
+                      @foreach($countries as $country)                      
+                      <option {{old('country') == $country ? 'selected':''}} value="{{$country}}">{{$country}}</option>
+                      @endforeach        
+                    </select>
+                  </div>
+                  <br><br><br><br>
                   <div class="input-group">
                     <input class="form-control nice-select @error('country') is-invalid @enderror"  required maxlength="50" style="width:45%" class=" arabic form-control " placeholder="المدينة" name="city" value="{{old('city')}}" type="text">
                   </div>
                   <br>
                   <div class="input-group">
                     <input class="form-control nice-select @error('mobile') is-invalid @enderror" required style="width:45%; margin-left: 10px;" class="form-control " placeholder="الجوال"  maxlength="14"  minlength="10" name="mobile" value="{{old('mobile')}}" type="text">
-                      
                     <input class="form-control nice-select"  required maxlength="10" minlength="10" style="width:45%" class=" arabic form-control " placeholder="رقم السجل المدني" name="identity" value="{{old('identity')}}" type="text">
                   </div>
                   <br>	
                   <div class="form-group mt-4 mb-4 @error('degree') is-invalid @enderror">
-                  <select name="degree" required class="form-control">
+                    <select name="degree" required class="form-control">
                       <option>الدرجة العلمية</option>
-<option {{old('degree') == 'ماجستير' ? 'selected':''}} value="ماجستير">ماجستير</option>
-                    <option {{old('degree') == 'دكتوارة' ? 'selected':''}} value="دكتوراة">دكتوراة</option>
-</select>
-               </div>
-                    <br><br><br><br>    
+                      <option {{old('degree') == 'ماجستير' ? 'selected':''}} value="ماجستير">ماجستير</option>
+                      <option {{old('degree') == 'دكتوارة' ? 'selected':''}} value="دكتوراة">دكتوراة</option>
+                    </select>
+                  </div>
+                  <br><br><br><br>    
                   <div class="input-group">
                     <input class="form-control nice-select @error('major') is-invalid @enderror"  required maxlength="50" style="width:45%" class=" arabic form-control " placeholder="التخصص" name="major" value="{{old('major')}}" type="text">
                   </div>
@@ -97,7 +96,7 @@ $countries = [
                     <input class="form-control nice-select @error('workplace') is-invalid @enderror" required minlength="2" maxlength="50" style="width:45%; margin-left: 10px;"  placeholder="مكان العمل" name="workplace" value="{{old('workplace')}}" type="text">
                     <input class="form-control nice-select @error('job_title') is-invalid @enderror"  required maxlength="50" style="width:45%" class=" arabic form-control " placeholder="المسمى الوظيفي" name="job_title" value="{{old('job_title')}}" type="text">
                   </div>
-                    <br>
+                  <br>
                   <div class="input-group">
                     <input class="form-control nice-select @error('email') is-invalid @enderror" required minlength="2" maxlength="50" style="width:45%; margin-left: 10px;" class="form-control " placeholder="البريد الإلكتروني" name="email" value="{{old('email')}}" type="text">
                     <input class="form-control nice-select @error('email_confirmation') is-invalid @enderror"  required maxlength="50" style="width:45%" class=" arabic form-control " placeholder="تأكيد البريد الإلكتروني" name="email_confirmation" value="{{old('email_confirmation')}}" type="text">
@@ -108,19 +107,18 @@ $countries = [
                     <input class="form-control nice-select @error('password_confirmation') is-invalid @enderror"  required minlength="8" style="width:45%" class=" arabic form-control " placeholder="تأكيد كلمة المرور" name="password_confirmation" value="" type="password">
                   </div>
                   <br>									
-                    
-                <div class="form-group mt-4 mb-4">
-                <div class="captcha">
-                    <span>{!! captcha_img() !!}</span>
-                    <button type="button" class="btn btn-danger" class="reload" id="reload">
-                        &#x21bb;
-                    </button>
-                </div>
-               </div>
-              <div class="form-group mb-4">
-                <input id="captcha" type="text" class="form-control nice-select @error('captcha') is-invalid @enderror" placeholder="فضلًا، أدخل النص الموجود بالصورة" name="captcha">
-              </div> 
-                <br><br><br><br>
+                  <div class="form-group mt-4 mb-4">
+                    <div class="captcha">
+                      <span>{!! captcha_img() !!}</span>
+                      <button type="button" class="btn btn-danger" class="reload" id="reload">
+                      &#x21bb;
+                      </button>
+                    </div>
+                  </div>
+                  <div class="form-group mb-4">
+                    <input id="captcha" type="text" class="form-control nice-select @error('captcha') is-invalid @enderror" placeholder="فضلًا، أدخل النص الموجود بالصورة" name="captcha">
+                  </div>
+                  <br><br><br><br>
                   <div class="form-group">
                     <div class="col-lg-3">
                       <button type="submit" class="btn cmn-btn">تسجيل</button>
@@ -136,15 +134,15 @@ $countries = [
   </div>
 </div>
 <script type="text/javascript">
-    $('#reload').click(function () {
-        $.ajax({
-            type: 'GET',
-            url: 'reload-captcha',
-            success: function (data) {
-                $(".captcha span").html(data.captcha);
-            }
-        });
-    });
-
+  $('#reload').click(function () {
+      $.ajax({
+          type: 'GET',
+          url: 'reload-captcha',
+          success: function (data) {
+              $(".captcha span").html(data.captcha);
+          }
+      });
+  });
+  
 </script>
 @endsection
